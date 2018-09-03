277f86165f6e2c127335c107842c2a7be8b6580c
lavanyachitra <lavanya19399@gmail.com>
2018-09-03 14:10:16 +0530

    Create Largest.c

---
+++ Largest.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+if((a>c)&&(a>b))
+{
+printf("a is greater");
+}
+elseif((b>a)&&(b>c))
+{
+printf("b is greater");
+}
+else
+{
+printf("c is greater");
+}
+}
